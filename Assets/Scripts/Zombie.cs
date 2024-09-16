e0ebcceb14260ae3a8c7fd22d59412075cf5e94f
Rozcowork <ethanorozco258@gmail.com>
2024-09-16 16:55:53 -0400

    Created Zombie spawning

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Zombie : MonoBehaviour
 {
+    //
     public GameObject objectPrefab;
-
+    //
     public float xBound;
     public float yBound;
+    public float xBound1;
+    public float yBound1;
+    public float xBound2;
+    public float yBound2;
+    public float xBound3;
+    public float yBound3;
+    //
     public float spawnTimer;
-    public float spawnInterval;
+    public float spawnInterval = 1f;
+    public Transform playerPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +30,63 @@ public class Zombie : MonoBehaviour
     void Update()
     {
         spawnTimer += Time.deltaTime;
-        if (spawnTimer >= spawnTimer)
+        if (spawnTimer >= spawnInterval)
         {
             spawnTimer = 0f;
-            //Instantiate(objectPrefab, GetRandomPosition(), Quaternion.identity);
+            Instantiate(objectPrefab, Spawn(), Quaternion.identity);
         }
     }
 
+    private Vector3 Spawn()
+    {
+        int random = (int)Mathf.Round(Random.Range(0, 4));
+        if (random == 0)
+        {
+            return SpawnRight();
+        } 
+
+        else if (random == 1) 
+        {
+            return SpawnLeft();
+        }
+
+        else if (random == 2) 
+        {
+            return SpawnTop();
+        }
+
+        else
+        {
+            return SpawnBottom();
+        }
+    }
+    private Vector3 SpawnRight()
+    {
+        float x = Random.Range(xBound1 + playerPos.position.x, xBound + playerPos.position.x);
+        float y = Random.Range(yBound3 + playerPos.position.y, yBound + playerPos.position.y);
+        return new Vector3(x, y, 0);
+       
+    }
     
-   
+    private Vector3 SpawnLeft()
+    {
+        float x = Random.Range(xBound2 + playerPos.position.x, xBound3 + playerPos.position.x);
+        float y = Random.Range(yBound3 + playerPos.position.y, yBound + playerPos.position.y);
+        return new Vector3(x, y, 0);
+    }
+
+    private Vector3 SpawnTop()
+    {
+        float x = Random.Range(xBound2 + playerPos.position.x, xBound + playerPos.position.x);
+        float y = Random.Range(yBound1 + playerPos.position.y, yBound + playerPos.position.y);
+        return new Vector3(x, y, 0);
+    }
+
+    private Vector3 SpawnBottom()
+    {
+        float x = Random.Range(xBound2 + playerPos.position.x, xBound + playerPos.position.x);
+        float y = Random.Range(yBound3 + playerPos.position.y, yBound2 + playerPos.position.y);
+        return new Vector3(x, y, 0);
+    }
+
 }
