9511a793a270b5c3a525669d8e460b9d3cc8ace7
Rozcowork <ethanorozco258@gmail.com>
2024-11-25 16:48:16 -0500

    Created Projectile LaunchPoints and Prefabs

--- Assets/Scripts/ProjectileLaunch.cs
+++ Assets/Scripts/ProjectileLaunch.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.TerrainTools;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ProjectileLaunch : MonoBehaviour
 {
     //GLOBAL VARIABLES
     public GameObject projectilePrefab; //call the projectile prefab
+    public GameObject projectilePrefab1;
     public Transform launchPoint; //call the position of the Launch Point
-
+    public Transform launchPoint1;
+    public Transform launchPoint2;
+    public Transform launchPoint3;
+    public Transform launchPoint4;
+    public Transform launchPoint5;
+    public Transform launchPoint6;
+    public Transform launchPoint7;
     //COOLDOWNTIMER STUFF
-    public float shootTime = 2f; //how long the time is before you can launch the projectile
+    public float shootTime = 10f; //how long the time is before you can launch the projectile
     public float shootCount; //the timer on the shot
 
     public enum stateMode
     {
         RED,
-        GRAY,
         BLUE,
     }
     public stateMode myMode;
@@ -28,12 +35,16 @@ public class ProjectileLaunch : MonoBehaviour
         myRend = GetComponent<Renderer>();
         myMat = myRend.material;
         shootCount = shootTime; //at the beginning the you have to wait 0.5s before shooting
-        
+        InvokeRepeating("Shoot", shootTime, shootCount);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetMouseButtonDown(1))
+        {
+            
+        }
         switch (myMode)
         {
             case stateMode.RED:
@@ -44,19 +55,26 @@ public class ProjectileLaunch : MonoBehaviour
                 myMat.color = Color.blue;
                 break;
 
-            case stateMode.GRAY:
-                myMat.color = Color.gray;
-                break;
-
         }
 
        // if(myMode!= prevMode)
+    }
+    void Shoot()
+    {
+        if (myMode == stateMode.RED)
+        {
+            Explode();
+        }
+        else if (myMode == stateMode.BLUE)
+        {
+            Laser();
+        }
+
     }
 
     void EnterRed()
     {
         myMat.color = Color.red;
-        InvokeRepeating("Explode", shootTime, shootCount);
     }
 
     void EnterBlue()
@@ -72,15 +90,20 @@ public class ProjectileLaunch : MonoBehaviour
     private void Explode()
     {
         Instantiate(projectilePrefab, launchPoint.position, Quaternion.identity);
-        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Zombie"))
-        {
-            float d = Vector3.Distance(transform.position, obj.transform.position);
-            if (d <= 7)
-            {
-                Explode();
-            }
-            foreach (Zombie z in FindObjectsOfType<Zombie>()) { GameObject o = z.gameObject; }
+        Instantiate(projectilePrefab, launchPoint1.position, Quaternion.identity);
+        Instantiate(projectilePrefab, launchPoint2.position, Quaternion.identity);
+        Instantiate(projectilePrefab, launchPoint3.position, Quaternion.identity);
+        Instantiate(projectilePrefab, launchPoint4.position, Quaternion.identity);
+        Instantiate(projectilePrefab, launchPoint5.position, Quaternion.identity);
+        Instantiate(projectilePrefab, launchPoint6.position, Quaternion.identity);
+        Instantiate(projectilePrefab, launchPoint7.position, Quaternion.identity);
 
-        }
+    }
+    private void Laser()
+    {
+        Instantiate(projectilePrefab1, launchPoint1.position, Quaternion.identity);
+        Instantiate(projectilePrefab1, launchPoint3.position, Quaternion.identity);
+        Instantiate(projectilePrefab1, launchPoint5.position, Quaternion.identity);
+        Instantiate(projectilePrefab1, launchPoint7.position, Quaternion.identity);
     }
 }
