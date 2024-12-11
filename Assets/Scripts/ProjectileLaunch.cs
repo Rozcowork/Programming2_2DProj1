401cf4b63e393598e530f96282e8589f557eed5e
Rozcowork <ethanorozco258@gmail.com>
2024-12-11 16:55:02 -0500

    Updated Projectile Launch

--- Assets/Scripts/ProjectileLaunch.cs
+++ Assets/Scripts/ProjectileLaunch.cs
@@ -1,11 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
-using UnityEditor.TerrainTools;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.UIElements;
+
 
 public class ProjectileLaunch : MonoBehaviour
 {
